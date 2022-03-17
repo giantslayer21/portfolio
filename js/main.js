c2237d6a7b3fc4ec5e4958807db7490bfecd431e
thebenezer <samebenezer21@gmail.com>
2022-03-17 19:32:00 +0530

    character movement achieved

--- js/main.js
+++ js/main.js
@@ -25,8 +25,14 @@ const debugObject = {
 */
 const canvas = document.querySelector('#canvas' );
 let stats,info,plane;
-let camera, scene, renderer,controls,materia;
+let camera, scene, renderer,controls,material;
 let actions,mixer,activeAction, previousAction;
+
+let characterControllerInstance;
+const DIRECTIONS=['ArrowUp','ArrowDown','ArrowLeft','ArrowRight'];
+const keysPressed = {ArrowUp:false,ArrowDown:false,ArrowLeft:false,ArrowRight:false, ' ':false};
+let shiftToggle=false;
+
 const clock=new THREE.Clock();
 function hasWebGL() {
     const gl =canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
@@ -61,7 +67,6 @@ function init() {
     const near = 0.1;
     const far = 500;
     camera = new THREE.PerspectiveCamera( fov, aspect, near, far);
-    camera.position.set(20,6,10);
 
     // ***** SCENE & FOG ****** //
     scene = new THREE.Scene();
@@ -80,16 +85,17 @@ function init() {
     const textureLoader = new THREE.TextureLoader()
     const loader = new GLTFLoader();
 
+    setupOrbitControls();
 
 
     // ***** GEOMETRIES ****** //
-    const sphereGeometry = new THREE.SphereGeometry( 5, 32, 32 );
-    const sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
-    const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
-    sphere.position.set(0,20,20)
+    // const sphereGeometry = new THREE.SphereGeometry( 5, 32, 32 );
+    // const sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
+    // const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+    // sphere.position.set(0,20,10)
     // sphere.castShadow = true; //default is false
     // sphere.receiveShadow = false; //default
-    scene.add( sphere );
+    // scene.add( sphere );
 
     const geometry = new THREE.PlaneBufferGeometry( 1024, 1024 );
     plane = new THREE.Mesh( geometry, new THREE.MeshStandardMaterial({color: 0x22243a}) );
@@ -116,7 +122,7 @@ function init() {
         {
             child.material = bakedMaterial;
         })
-        gltf.scene.position.set(0,-2,0)
+        gltf.scene.scale.set(2,2,2)
         scene.add( gltf.scene );
     
     }, undefined, function ( error ) {
@@ -129,11 +135,10 @@ function init() {
     loader.load( './assets/Character/character3.glb', function ( gltf ) {
 
         const character = gltf.scene;
-        // character.position.set(0,20,0)
+        character.position.set(-4,4,40)
+        character.scale.set(0.5,0.5,0.5)
         scene.add( character );
-        mixer = new THREE.AnimationMixer( character );
-
-        setupCharacterAnimations( character, gltf.animations );
+        characterControllerInstance=new CharacterController(character,gltf.animations,camera,controls)
     }, undefined, function ( e ) {
 
         console.error( e );
@@ -141,7 +146,6 @@ function init() {
     });
 
     
-    orbitalcontrols();
     window.addEventListener( 'resize', onWindowResize,false );
 
 }
@@ -159,22 +163,14 @@ function onWindowResize() {
 }
 
 
-function render(time) {
+function render() {
 
-    // material.uniforms.uTime.value = time;
     const dt = clock.getDelta();
-    if ( mixer ){
-        mixer.update( dt );
-        if(DIRECTIONS.some(key=>keysPressed[key] == true)){
-            if(shiftToggle)fadeToAction('run')
-            else fadeToAction('walk')
-        }
-        else{
-            fadeToAction('idle')
-        }
-        if (keysPressed[' '])fadeToAction('jump_start')
-    } 
+    if ( characterControllerInstance ){
+        characterControllerInstance.update(keysPressed,shiftToggle,dt);
+    // console.log(characterControllerInstance.camera)
 
+    } 
     renderer.render( scene, camera );
     controls.update();// only required if controls.enableDamping = true, or if controls.autoRotate = true
     stats.update();
@@ -182,74 +178,164 @@ function render(time) {
 }
 
 
-const DIRECTIONS=['ArrowUp','ArrowDown','ArrowLeft','ArrowRight']
-const keysPressed = {ArrowUp:false,ArrowDown:false,ArrowLeft:false,ArrowRight:false, ' ':false}
-let shiftToggle=false;
 document.addEventListener('keydown', (e) => {
-        shiftToggle=e.shiftKey
+        shiftToggle=e.shiftKey;
         if (e.key in keysPressed){
-            keysPressed[e.key]=true
+            keysPressed[e.key]=true;
         }
     }, false);
 document.addEventListener('keyup', (e) => {
-        shiftToggle=e.shiftKey
-        // console.log(e)
+        shiftToggle=e.shiftKey;
         if (e.key in keysPressed){
-            keysPressed[e.key]=false
+            keysPressed[e.key]=false;
         } 
     }, false);
 
 class CharacterController{
-    constructor(){
 
+    // temporary data
+    walkDirection = new THREE.Vector3()
+    rotateAngle = new THREE.Vector3(0, 1, 0)
+    rotateQuarternion= new THREE.Quaternion()
+    cameraTarget = new THREE.Vector3()
+    
+    // constants
+    fadeDuration= 0.2
+    runVelocity = 20
+    walkVelocity = 7
+
+    constructor(character,animations,camera,orbitControls){
+        this.character = character;
+        this.actions = {};
+        this.mixer = new THREE.AnimationMixer( character );
+        this.setupCharacterAnimations(animations );
+        this.currentAction='idle'
+        this.camera = camera;
+        this.camera.position.x=this.character.position.x;
+        this.camera.position.y=this.character.position.y+4;
+        this.camera.position.z=this.character.position.z+10;
+        this.orbitControls = orbitControls;
+        this.updateCameraTarget(0,0);
     }
+
+    update( keysPressed, shiftToggle,delta) {
+
+        const directionPressed = DIRECTIONS.some(key => keysPressed[key] == true)
+        var play = 'idle';
+        if (directionPressed && shiftToggle) {
+            play = 'run'
+        } else if (directionPressed) {
+            play = 'walk'
+        }
+
+        this.fadeToAction(play)
+        this.mixer.update(delta)
+
+        if (this.currentAction == 'run' || this.currentAction == 'walk') {
+            // calculate towards camera direction
+            var angleYCameraDirection = Math.atan2(
+                    (this.camera.position.x - this.character.position.x), 
+                    (this.camera.position.z - this.character.position.z))
+            // diagonal movement angle offset
+            var directionOffset = this.directionOffset(keysPressed)
+
+            // rotate character
+            this.rotateQuarternion.setFromAxisAngle(this.rotateAngle, angleYCameraDirection + directionOffset)
+            this.character.quaternion.rotateTowards(this.rotateQuarternion, 0.2)
+
+            // calculate direction
+            this.camera.getWorldDirection(this.walkDirection)
+            this.walkDirection.y = 0
+            this.walkDirection.normalize()
+            this.walkDirection.applyAxisAngle(this.rotateAngle, directionOffset)
+
+            // run/walk velocity
+            const velocity = this.currentAction == 'run' ? this.runVelocity : this.walkVelocity
+
+            // move character & camera
+            const moveX = -this.walkDirection.x * velocity * delta
+            const moveZ = -this.walkDirection.z * velocity * delta
+            this.character.position.x += moveX
+            this.character.position.z += moveZ
+            this.updateCameraTarget(moveX, moveZ)
+        }
+    }
+    updateCameraTarget(moveX, moveZ) {
+        // move camera
+        this.camera.position.x += moveX
+        this.camera.position.z += moveZ
+
+        // update camera target
+        this.cameraTarget.x = this.character.position.x
+        this.cameraTarget.y = this.character.position.y + 1
+        this.cameraTarget.z = this.character.position.z
+        this.orbitControls.target = this.cameraTarget
+    }
+    fadeToAction( actionName ) {
+
+        const previousAction = this.currentAction;
+        this.currentAction = actionName;
+    
+        if ( previousAction !== this.currentAction ) {
+    
+            this.actions[previousAction].fadeOut(0.5);
+            this.actions[this.currentAction]
+            .reset()
+            .setEffectiveTimeScale( 1 )
+            .setEffectiveWeight( 1 )
+            .play();
+        }
+    }
+    setupCharacterAnimations( animations ) {
+        // const states = [ 'catch','death','fall','guard','hit','hit_guard','idle','interact','jump_start','pull','push','put','run','throw','walk'];
+        for ( let i = 0; i < animations.length; i ++ ) {
+            const clip = animations[ i ];
+            const action = this.mixer.clipAction( clip );
+            this.actions[ clip.name ] = action;    
+        }
+        this.currentAction = 'idle' ;
+        this.actions[this.currentAction].play();
+    }
+    directionOffset(keysPressed) {
+        var directionOffset = 0 // w
+
+        if (keysPressed['ArrowDown']) {
+            if (keysPressed['ArrowRight']) {
+                directionOffset  = Math.PI / 4 // w+a
+            } else if (keysPressed['ArrowLeft']) {
+                directionOffset = - Math.PI / 4 // w+d
+            }
+        } else if (keysPressed['ArrowUp']) {
+            if (keysPressed['ArrowRight']) {
+                directionOffset = Math.PI / 4 + Math.PI / 2 // s+a
+            } else if (keysPressed['ArrowLeft']) {
+                directionOffset = -Math.PI / 4 - Math.PI / 2 // s+d
+            } else {
+                directionOffset = Math.PI // s
+            }
+        } else if (keysPressed['ArrowRight']) {
+            directionOffset = Math.PI / 2 // a
+        } else if (keysPressed['ArrowLeft']) {
+            directionOffset = - Math.PI / 2 // d
+        }
+
+        return directionOffset
+    }
+
 }
 
 
 
 
 
-function setupCharacterAnimations( character, animations ) {
 
-    // const states = [ 'catch','death','fall','guard','hit','hit_guard','idle','interact','jump_start','pull','push','put','run','throw','walk'];
-                
-    actions = {};
 
-    for ( let i = 0; i < animations.length; i ++ ) {
 
-        const clip = animations[ i ];
-        const action = mixer.clipAction( clip );
-        actions[ clip.name ] = action;
-        console.log(clip.name)
 
-    }
-    activeAction = actions[ 'idle' ];
-    activeAction.play();
-
-}
-
-function fadeToAction( name ) {
-
-    previousAction = activeAction;
-    activeAction = actions[ name ];
-
-    if ( previousAction !== activeAction ) {
-
-        previousAction.fadeOut(0.5);
-        activeAction
-        .reset()
-        .setEffectiveTimeScale( 1 )
-        .setEffectiveWeight( 1 )
-        .play();
-        // activeAction.crossFadeFrom(previousAction,0.5,true).play();
-
-    }
-}
-
-function orbitalcontrols() {
+function setupOrbitControls() {
     // Setup orbital controls
     controls = new OrbitControls(camera, renderer.domElement);
-    controls.listenToKeyEvents( window ); // optional
+    // controls.listenToKeyEvents( window ); // optional
 
     controls.enableKeys = false;
     controls.enablePan = false;
@@ -260,6 +346,7 @@ function orbitalcontrols() {
     // controls.minDistance = 200;
     // controls.maxDistance = 350;
     controls.maxPolarAngle = Math.PI/2;
+    controls.update();
 
 }
 
@@ -267,11 +354,11 @@ function guiPanel() {
     const gui=new dat.GUI();
     gui.addColor(debugObject,'scenecolor')
     .onChange(()=>{
-        scene.background.set(debugObject.scenecolor)
+        scene.background.set(debugObject.scenecolor);
     });
     gui.addColor(debugObject,'ambientlight')
     .onChange(()=>{
-        scene.children[0].color.set(debugObject.ambientlight)
+        scene.children[0].color.set(debugObject.ambientlight);
     });
     
 }
